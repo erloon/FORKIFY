20881180c2e498f53fd4b72a89c859da01058287
Maciej Kryca <maciej.kryca@gmail.com>
2018-06-29 21:35:59 +0200

    search controler cd

--- src/js/index.js
+++ src/js/index.js
@@ -1,5 +1,25 @@
 import Search from './models/Search';
 
-const search = new Search("pizza");
-search.getResults();
-console.log(search.query);
+const state = {}
+
+const controlSearch = async () => {
+    const query = 'pizza';
+    if (query) {
+
+        state.search = new Search(query);
+
+        // przygotowanie do wyników
+
+        //wyszukanie przepisów
+        await state.search.getResults();
+
+        //wyniki na UI
+        console.log(state.search.result);
+    }
+}
+
+
+document.querySelector('.search').addEventListener('submit', e => {
+    e.preventDefault();
+    controlSearch();
+});
