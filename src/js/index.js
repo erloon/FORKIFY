28cc3c8da5f7bf18612efa548a8795cc8d65d079
Maciej Kryca <maciej.kryca@gmail.com>
2018-08-04 21:44:56 +0200

    Added paging

--- src/js/index.js
+++ src/js/index.js
@@ -20,7 +20,7 @@ const controlSearch = async () => {
         //wyszukanie przepisów
         await state.search.getResults();
 
-        clearRender()
+        clearRender();
         //wyniki na UI
         searchView.renderResults(state.search.result);
     }
@@ -31,3 +31,13 @@ elements.searchForm.addEventListener('submit', e => {
     e.preventDefault();
     controlSearch();
 });
+
+elements.searchResPages.addEventListener('click',e=>{
+    const btn = e.target.closest('.btn-inline');
+    if(btn){
+        const goTo = parseInt(btn.dataset.goto,10);
+        searchView.clearResults();
+        searchView.renderResults(state.search.result,goTo);
+    }
+    
+})
